68d32fa301ac0e063b55488d37576f2f5c5d3e9d
Giovanni Sakti <giosakti@starqle.com>
2012-06-26 12:11:02 +0700

    added ability to create unix socket upstream

--- manifests/resource/upstream.pp
+++ manifests/resource/upstream.pp
@@ -5,6 +5,7 @@
 # Parameters:
 #   [*ensure*]      - Enables or disables the specified location (present|absent)
 #   [*members*]     - Array of member URIs for NGINX to connect to. Must follow valid NGINX syntax.
+#   [*unix*]				- Valid NGINX syntax for unix socket
 #
 # Actions:
 #
@@ -19,21 +20,42 @@
 #        'localhost:3002',
 #    ],
 #  }
+#  
+#  nginx::resource::upstream { 'proxypass':
+#		 ensure => present,
+#		 socket => "unix:/tmp/unicorn.todo.sock fail_timeout=0"}
+#	 }
+
 define nginx::resource::upstream (
 	$ensure 	= 'present',
-	$members
+	$members,
+	$socket
 ){
 	File { 
 		owner => 'root', 
 		group => 'root', 
 		mode  => '0644', 
 	}
-	file { "/etc/nginx/conf.d/${name}-upstream.conf": 
-		ensure => $ensure ? {
-			'absent' => absent,
-			default	 => 'file',
-		},
-		content => template('nginx/conf.d/upstream.erb'),
-		notify => Class['nginx::service'],
+
+	if ($members != undef) {
+		file { "/etc/nginx/conf.d/${name}-upstream.conf": 
+			ensure => $ensure ? {
+				'absent' => absent,
+				default	 => 'file',
+			},
+			content => template('nginx/conf.d/upstream_port.erb'),
+			notify => Class['nginx::service'],
+		}
+	} else {
+		file { "/etc/nginx/conf.d/${name}-upstream.conf": 
+			ensure => $ensure ? {
+				'absent' => absent,
+				default	 => 'file',
+			},
+			content => template('nginx/conf.d/upstream_socket.erb'),
+			notify => Class['nginx::service'],
+		}
 	}
+	
+	
 }
